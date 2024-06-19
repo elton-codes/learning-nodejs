d179abb977eec2c4b49d196c9b8a6df6e9249b99
elton asamoah <eltonasamoah007@gmail.com>
2024-06-19 09:27:40 +0000

    learning-nodejs

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import {createServer} from 'node:http';
+
+const server = createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Server is running');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server is listening');
+});
+
+//Arrow Functions 
+function handleRequest() {}
+const handleRequest = () => {}
